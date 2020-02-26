fa2edbcbe93664ca35e0a1a2d4f06b8b2f066728
renwickp <peter2renwick@gmail.com>
2020-02-26 15:48:21 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import List from './List';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import List from "./List";
 
 function App() {
+  const [todos, setTodos] = useState(["todo1", "todo2"]);
   return (
-    <List />
+    <>
+      <List toDoList={todos} />
+
+      <input type="text" />
+      <button> Add</button>
+      <button> Clear </button>
+      <div>0 left</div>
+    </>
   );
 }
 
